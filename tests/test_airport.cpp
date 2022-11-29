7990430835df0b9625864830b4a7781fc33749a6
nicolemiao <xinmiao3@illinois.edu>
2022-11-29 20:14:07 +0000

    Finished test cases for airport reader

--- tests/test_airport.cpp
+++ tests/test_airport.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 
 TEST_CASE("Test Airport Reader 1","[weight=1][part=1]") {
-    //unordered_map<string, pair<long double, long double>> airports_list;
     Airport airport;
     airport.read_airports_file("../tests/test_airports.dat");
 
@@ -17,26 +16,96 @@ TEST_CASE("Test Airport Reader 1","[weight=1][part=1]") {
     pair<long double, long double> in1 = airport.get_location("1");
     
     REQUIRE(test1.first - in1.first < 0.001);
-
-    //REQUIRE(airport.get_location("24") == pair<-56.083099365200006, 51.3918991089>);
-
+    REQUIRE(test1.second - in1.second < 0.001);
 
 }
 
-// TEST_CASE("Test Airport Reader 2","[weight=1][part=1]") {
-//     unordered_map<string, pair<long double, long double>> airports_list;
-//     read_airports_file("airports.dat");
+TEST_CASE("Test Airport Reader 2","[weight=1][part=1]") {
+    Airport airport;
+    airport.read_airports_file("../tests/test_airports.dat");
 
-//     REQUIRE(airports_list["2423"].get_location() == pair<123.014999, 10.7764>);
+    pair<long double, long double> test2;
+    test2.first = -81.2425;
+    test2.second = 68.77610015869999;
 
-//     REQUIRE(airports_list["2990"].get_location() == pair<49.278701782227, 55.606201171875>);
-// }
+    pair<long double, long double> in2 = airport.get_location("148");
+    
+    REQUIRE(test2.first - in2.first < 0.001);
+    REQUIRE(test2.second - in2.second < 0.001);
 
-// TEST_CASE("Test Airport Reader 3","[weight=1][part=1]") {
-//     unordered_map<string, pair<long double, long double>> airports_list;
-//     read_airports_file("airports.dat");
+}
 
-//     REQUIRE(airports_list["8888"].get_location() == pair<-72.27079772949219, 42.898399353027344>);
+TEST_CASE("Test Airport Reader 3","[weight=1][part=1]") {
+    Airport airport;
+    airport.read_airports_file("../tests/test_airports.dat");
 
-//     REQUIRE(airports_list["10952"].get_location() == pair<45.0125999451, 43.322299957300004>);
-// }
+    pair<long double, long double> test3;
+    test3.first = 9.45244;
+    test3.second = 24.292801;
+
+    pair<long double, long double> in3 = airport.get_location("211");
+    
+    REQUIRE(test3.first - in3.first < 0.001);
+    REQUIRE(test3.second - in3.second < 0.001);
+
+}
+
+TEST_CASE("Test Airport Reader 4","[weight=1][part=1]") {
+    Airport airport;
+    airport.read_airports_file("../tests/test_airports.dat");
+
+    pair<long double, long double> test4;
+    test4.first = -1.09582996368;
+    test4.second = 51.616401672399995;
+
+    pair<long double, long double> in4 = airport.get_location("555");
+    
+    REQUIRE(test4.first - in4.first < 0.001);
+    REQUIRE(test4.second - in4.second < 0.001);
+
+}
+
+TEST_CASE("Test Airport Reader 5","[weight=1][part=1]") {
+    Airport airport;
+    airport.read_airports_file("../tests/test_airports.dat");
+
+    pair<long double, long double> test5;
+    test5.first = 2.7605500221;
+    test5.second = 41.901000977;
+
+    pair<long double, long double> in5 = airport.get_location("1222");
+    
+    REQUIRE(test5.first - in5.first < 0.001);
+    REQUIRE(test5.second - in5.second < 0.001);
+
+}
+
+TEST_CASE("Test Airport Reader 6","[weight=1][part=1]") {
+    Airport airport;
+    airport.read_airports_file("../tests/test_airports.dat");
+
+    pair<long double, long double> test6;
+    test6.first = 149.610992432;
+    test6.second = -32.5625;
+
+    pair<long double, long double> in6 = airport.get_location("6808");
+    
+    REQUIRE(test6.first - in6.first < 0.001);
+    REQUIRE(test6.second - in6.second < 0.001);
+
+}
+
+TEST_CASE("Test Airport Reader 7","[weight=1][part=1]") {
+    Airport airport;
+    airport.read_airports_file("../tests/test_airports.dat");
+
+    pair<long double, long double> test7;
+    test7.first = -72.27079772949219;
+    test7.second = 42.898399353027344;
+
+    pair<long double, long double> in7 = airport.get_location("8888");
+    
+    REQUIRE(test7.first - in7.first < 0.001);
+    REQUIRE(test7.second - in7.second < 0.001);
+
+}
