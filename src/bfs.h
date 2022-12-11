440a890b993c027ce84152470368c3393f1a201d
Victor Li <59171403+VictorLiwentao@users.noreply.github.com>
2022-12-10 20:45:16 -0600

    Update bfs.h

--- src/bfs.h
+++ src/bfs.h
@@ -10,8 +10,6 @@ class BFS{
      public: 
         BFS();
         ~BFS();
-        //long double BFSdistance(string start, string end, Airport airport);
-        //long double BreadFirstSearch(string start, string end, Airport airport, vector<string> &bfs_list);
         vector<string> BreadFirstSearch(string start, Airport airport, vector<string> &bfs_list);
 };
    
