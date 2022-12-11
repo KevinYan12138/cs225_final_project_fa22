978ea21ef2c79f10e696879a29a825098a1d3536
Victor Li <59171403+VictorLiwentao@users.noreply.github.com>
2022-12-10 22:15:28 -0600

    added comments for BFS

--- src/bfs.h
+++ src/bfs.h
@@ -8,8 +8,20 @@ using namespace std;
 
 class BFS{
      public: 
+        /*
+         The constructor for the BFS class
+        */
         BFS();
+        /*
+        The destructor for the BFS class
+        */ 
         ~BFS();
+        /*
+        The BreadFirstSearch algorithm
+        It takes in a starting airport ID, the airport object we are going to traverse, and a vector of strings to store it
+        BreadFirstSearch will return all the airports that the we can arrive from the starting airport
+        These airports will be stored in a vector of airport IDs, the starting airport will be the element at the 0th index
+        */
         vector<string> BreadFirstSearch(string start, Airport airport, vector<string> &bfs_list);
 };
    
