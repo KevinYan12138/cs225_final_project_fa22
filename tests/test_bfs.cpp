d32928eff4f5a72ee3d5b80517abc6e928eb58fa
Victor Li <59171403+VictorLiwentao@users.noreply.github.com>
2022-12-11 13:18:44 -0600

    added BFS testcases with the whole dataset

--- tests/test_bfs.cpp
+++ tests/test_bfs.cpp
@@ -4,13 +4,39 @@
 #include "bfs.h"
 using namespace std;
 
+TEST_CASE("Test BreadFirstSearch routes"){
+    Airport airport;
+    airport.read_routes_file("../routes.dat");
+    vector<string> vec;
+    BFS bfs;
+    vector<string> traversal = bfs.BreadFirstSearch("2990",airport,vec);  
+    cout << "the size of vector after traverse the whole route file starting from 2990 is ->" << traversal.size() << endl;
+
+    for(size_t i = 0; i <traversal.size() ;i++){
+        for (size_t j = i+1; j < traversal.size();j++){
+            if (traversal[i]==traversal[j]){
+                cout << "not right" << endl;
+            }
+        }
+    }
+    for(size_t i = 0; i <traversal.size() ;i++){
+        for (size_t j = i+1; j < traversal.size();j++){
+            REQUIRE(traversal[i]!=traversal[j]);
+        }
+    }
+    cout << "" << endl;
+    REQUIRE(traversal.size()==3199);
+    REQUIRE(traversal.at(0)=="2990");
+
+}
 TEST_CASE("Test BreadFirstSearch exceptions 1"){
     Airport airport;
     airport.read_routes_file("../tests/testroutes.dat");
     vector<string> vec;
     BFS bfs;
-    cout << "traversal start with 0" << endl;
+    cout << "BFS traversal start with 0" << endl;
     vector<string> traversal = bfs.BreadFirstSearch("0",airport,vec);  
+   
     for(size_t i =0; i < vec.size() ;i++){
         cout << vec[i] << endl;
     }
@@ -23,9 +49,10 @@ TEST_CASE("Test BreadFirstSearch exceptions 2"){
     airport.read_routes_file("../tests/testroutes.dat");
     vector<string> vec;
     BFS bfs;
-    cout << "traversal start with 1111" << endl;
+    cout << "BFS traversal start with 1111" << endl;
     vector<string> traversal = bfs.BreadFirstSearch("1111",airport,vec);
     cout << "" << endl;
+
     for(size_t i =0; i < vec.size() ;i++){
         cout << vec[i] << endl;
     }
@@ -35,14 +62,14 @@ TEST_CASE("Test BreadFirstSearch exceptions 2"){
 TEST_CASE("Test BreadFirstSearch 1"){
     Airport airport;
     airport.read_routes_file("../tests/testroutes.dat");
-    airport.read_airports_file("../airports.dat");
     vector<string> vec;
     BFS bfs;
     vector<string> traversal = bfs.BreadFirstSearch("4029",airport,vec);
-    cout << "traversal start with 4029" << endl;
-    for(size_t i =0; i < traversal.size() ;i++){
+    cout << "BFS traversal start with 4029" << endl;
+   
+    for(size_t i =0; i < vec.size() ;i++){
         
-        cout << traversal[i] << endl;
+        cout << vec[i] << endl;
     }
     cout << "" << endl;
     REQUIRE(!traversal.empty());
@@ -52,24 +79,24 @@ TEST_CASE("Test BreadFirstSearch 1"){
     REQUIRE(traversal.at(2)=="1111");
    
 }
-// TEST_CASE("Test BreadFirstSearch 2"){
-//     Airport airport;
-//     airport.read_routes_file("../tests/testroutes.dat");
-//     vector<string> vec;
-//     BFS bfs;
-//     cout << "traversal start with 2968" << endl;
-//     vector<string> traversal = bfs.BreadFirstSearch("2968",airport,vec);
+TEST_CASE("Test BreadFirstSearch 2"){
+    Airport airport;
+    airport.read_routes_file("../tests/testroutes.dat");
+    vector<string> vec;
+    BFS bfs;
+    cout << "BFS traversal start with 2968" << endl;
+    vector<string> traversal = bfs.BreadFirstSearch("2968",airport,vec);
    
-//     for(size_t i =0; i < vec.size() ;i++){
+    for(size_t i =0; i < vec.size() ;i++){
        
-//         cout << vec[i] << endl;
-//     }
-//     cout << "" << endl;
-//     REQUIRE(!traversal.empty());
-//     REQUIRE(traversal.size() == 4);
-//     REQUIRE(traversal.at(0)=="2968");
-//     REQUIRE(traversal.at(1)=="2990");
-//     REQUIRE(traversal.at(2)=="4078");
-//     REQUIRE(traversal.at(3)=="1111");
-// }
+        cout << vec[i] << endl;
+    }
+    cout << "" << endl;
+    REQUIRE(!traversal.empty());
+    REQUIRE(traversal.size() == 4);
+    REQUIRE(traversal.at(0)=="2968");
+    REQUIRE(traversal.at(1)=="2990");
+    REQUIRE(traversal.at(2)=="4078");
+    REQUIRE(traversal.at(3)=="1111");
+}
 
