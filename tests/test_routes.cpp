2ecddc11591dd88af47db00353524e9560fc74a6
Victor Li <59171403+VictorLiwentao@users.noreply.github.com>
2022-12-11 17:54:27 -0600

    test_routes

--- tests/test_routes.cpp
+++ tests/test_routes.cpp
@@ -47,21 +47,3 @@ TEST_CASE("Test file reading 4"){
     test4.second = "410";
     REQUIRE(vec4[0] == test4);
 }
-
-// TEST_CASE("Test file reading 2" "FileR2"){
-//     read_routes_file("testroutes.dat");
-//     vector<pair<string,string>> vec2 = get_airport_adj("2966");
-//     REQUIRES(airport_adj_list["2966"][0]) == <2990, 410>;
-// }
-// TEST_CASE("Test file reading 3" "FileR3"){
-//     read_routes_file("testroutes.dat");
-//     vector<pair<string,string>> vec3 = get_airport_adj("2968");
-//     REQUIRES(airport_adj_list["2968"][0]) == <2990, 410>;
-//     REQUIRES(airport_adj_list["2968"][1]) ==  <4078,410>;
-    
-// }
-// TEST_CASE("Test file reading 4" "FileR4"){
-//     read_routes_file("testroutes.dat");
-//     vector<pair<string,string>> vec1 = get_airport_adj("4029");
-//     REQUIRES(airport_adj_list["4029"]) == <2990, 410>;
-// }
